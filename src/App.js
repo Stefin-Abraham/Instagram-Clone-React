ff8c4633ac3ff47408bbac0e860f4e89714ace7c
Stefin-Abraham <Abraham.stefin@gmail.com>
2023-01-20 18:51:39 +0530

    Feat : Created Home/Login/SignUp-Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+
 
 function App() {
   return (
-    <Container>
-      <h1>Hello</h1>
+    <Router>
+      <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </Container>
+    </Router>
   );
 }
 
