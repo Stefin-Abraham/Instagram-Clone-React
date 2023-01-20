b0c1c9a40fe9162c5ee1b6d78b300213eb49e0ac
Stefin-Abraham <Abraham.stefin@gmail.com>
2023-01-20 21:34:04 +0530

    Feat: Login & SignUp Page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,115 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
+    <Container>
+        <Main>
+            <Form>
+                <Logo>
+                    <img src = "./instagram-text-logo.png" alt="" />
+                </Logo>
+                <InputContainer>
+                    <input type = "email"placeholder="Email" />
+                </InputContainer>
+                <InputContainer>
+                    <input type = "text"placeholder="Username" />
+                </InputContainer>
+                <InputContainer>
+                    <input type = "password"placeholder="Password" />
+                </InputContainer>
+                <InputContainer>
+                    <input type = "text"placeholder="PhotoURL (Optional)" />
+                </InputContainer>
+                
+                <button>
+                    Sign Up
+                </button>
+            </Form>
+            <SignUpContainer>
+                <p> 
+                  Have an account ? <span>Log In</span>
+                </p>
+            </SignUpContainer>
+        </Main>
+    </Container>
   )
 }
 
-export default SignUp
+const Container = styled.div`
+ width: 100vw;
+ height: 100vh;
+ display: flex;
+ align-items: center;
+ justify-content: center;
+
+`;
+
+const Main = styled.div``;
+
+const Form = styled.form`
+    background: #fff;
+    border: 1px solid lightgray;
+    padding: 20px;
+    min-width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 400px;
+
+  button {
+    height: 33px;
+    width: 230px;
+    background-color: #0095f6;
+    border: none;
+    outline: none;
+    border-radius: 5px;
+    margin-top: 30px;
+    font-size: 14px;
+    color: #fff;
+    cursor: pointer;   
+  }
+`;
+
+const Logo = styled.div`
+  width: 250px;
+  img{
+    width: 100%;
+ }
+`;
+
+const  InputContainer = styled.div`
+ height: 25px;
+ width: 250px;
+ margin-top: 20px;
+ input {
+  height: 100%;
+  width: 100%;
+  background-color: #fafafa;
+  border: 1px solid gray;
+  padding: 5px;
+}
+`;
+
+const SignUpContainer = styled.div`
+  border: 1px solid lightgray;
+  padding: 20px;
+  background-color: #fff;
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    font-size: 14px;
+
+    span {
+      color: #18a4f8;
+      font-weight: 600;
+      cursor: pointer;
+    }
+  }
+`;
+
+export default SignUp;
