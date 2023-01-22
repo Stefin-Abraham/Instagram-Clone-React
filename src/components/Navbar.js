6e497c2e79f6f3ad0c8cbacee89201d396301d07
Stefin-Abraham <abraham.stefin@gmail.com>
2023-01-23 01:18:54 +0530

    Feat: Instlld Material UI/Log In-Out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 import { signOut } from "firebase/auth";
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { auth } from "./firebase";
-import { useStateValue } from "../StateProvider";
 import { useNavigate } from "react-router-dom";
-
+import styled from 'styled-components';
+import db, { auth } from "./firebase";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useStateValue } from "../StateProvider";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 function Navbar() {
    const [{ user }, dispatch] = useStateValue();
    const [openMenu, setOpenMenu] = useState(false);
+   const [openDialog, setOpenDialog] = useState(false);
+   const [imageURL, setImageURL] = useState("");
+   const [caption, setCaption] = useState("");
    const navigate = useNavigate();
    const logOut = (e) => {
     signOut(auth)
@@ -21,8 +28,61 @@ function Navbar() {
       })
       .catch((err) => alert(err));
   };
+
+  const createPost = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, "posts"), {
+      caption,
+      imageURL,
+      userName: user?.userName,
+      photoURL: user?.photoURL === null ? "./user.png" : user?.photoURL,
+      timeStamp: serverTimestamp(),
+    });
+    alert("Post created 🚀🚀🚀");
+    setCaption("");
+    setImageURL("");
+    setOpenDialog(false);
+  };
   return (
     <Container>
+      <Dialog 
+          open= {openDialog} 
+          onClose={() => setOpenDialog(false)} 
+          maxWidth="md"
+          fullWidth
+        >
+        <DialogTitle>Create a Post</DialogTitle>
+        <DialogContent>
+          <CreatePostForm>
+            <InputContainer>
+              <input 
+                type="text" 
+                placeholder="ImageURL" 
+                value={imageURL}
+                onChange={(e) => setImageURL(e.target.value)} 
+              />
+            </InputContainer>
+            <InputContainer>
+              < textarea 
+                type="text" 
+                placeholder="caption"
+                value={caption}
+                onChange={(e) => setCaption(e.target.value)}
+               />
+            </InputContainer>
+          </CreatePostForm>
+        </DialogContent>
+        <DialogActions>
+          <PostCTAButtons>
+            <button 
+              className="cancel-button" 
+              onClick={() => setOpenDialog(false)}>Cancel</button>
+            <button 
+              className="post-button" 
+              onClick={createPost}>Post</button>
+          </PostCTAButtons>
+        </DialogActions>
+      </Dialog>
         <Logo>
             <img src="./logo.png" alt ="/" />
         </Logo>
@@ -31,13 +91,13 @@ function Navbar() {
         </SearchBar>
         <Icons>
             <Icon>
-                <img src="./41-home.svg" alt="/" />
+                <img src="./41-home.svg" alt="" />
             </Icon>
             <Icon>
-                <img src= "./40-add-card.svg" alt="/" />
+                <img src= "./40-add-card.svg" alt="" onClick={() => setOpenDialog(true)} />
             </Icon> 
             <Icon>
-                <img src="./47-chat (1).svg" alt="/" />
+                <img src="./47-chat (1).svg" alt="" />
             </Icon>
              <Icon>
                 <img 
@@ -58,23 +118,23 @@ function Navbar() {
   );
 }
 const Container = styled.div`
-    height: 60px;
-    padding-top: 5px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
+  height: 60px;
+  padding-top: 5px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
 
-    border-bottom: 1px solid lightgray;
-    background-color: #fff;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 100;
+  border-bottom: 1px solid lightgray;
+  background-color: #fff;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 100;
 
-    @media only screen and (max-width: 768px) {
+  @media only screen and (max-width: 768px) {
     justify-content: space-around;
-    }
+  }
 
 `;
 
@@ -83,39 +143,38 @@ const Logo = styled.div`
 `;
 
 const SearchBar = styled.div`
-    height: 30px;
-    width: 268px;
-    padding: 3px 16px 3px 16px;
-    min-height: auto;
-    min-width: auto;
-    background-color: #efefef;
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  height: 30px;
+  width: 268px;
+  padding: 3px 16px 3px 16px;
+  min-height: auto;
+  min-width: auto;
+  background-color: #efefef;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    input {
-      background-color: transparent;
-      border: none;
-      outline: none;
-      line-height: 18px;
-      font-size: 14px;
-      width: 90%;
-    }
+  input {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    line-height: 18px;
+    font-size: 14px;
+    width: 90%;
+  }
 
-    @media only screen and (max-width: 768px) {
-      display: none;
-    }
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
 
 `;
 
 const Icons = styled.div`
-    display: flex;
-    align-items: center;
-    width: 180px;
-    justify-content: space-evenly;
-    height: 40px;
-    
+  display: flex;
+  align-items: center;
+  width: 180px;
+  justify-content: space-evenly;
+  height: 40px;    
 `;
 
 const Icon = styled.div`
@@ -132,7 +191,7 @@ const Icon = styled.div`
     img {
       border-radius: 50%;
     }
-      position: relative;
+    position: relative;
   }
 
 `;
@@ -154,5 +213,53 @@ const MenuElement = styled.div`
     background-color: #e4e4e4;
   }
 `;
+const CreatePostForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  align-items: center;
+  height: 300px;
+`;
 
+const InputContainer = styled.div`
+  width: 90%;
+  height: 33px;
+  margin-bottom: 20px;
+  input {
+    width: 100%;
+    height: 100%;
+    border: 1px solid lightgrey;
+    padding: 5px;
+    outline: none;
+  }
+
+  textarea {
+    width: 100%;
+    height: 200px;
+    resize: none;
+    border: 1px solid lightgrey;
+    padding: 5px;
+    outline: none;
+  }  
+`;
+const PostCTAButtons = styled.div`
+  button {
+    width: 100px;
+    height: 33px;
+    margin-right: 10px;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    color: #fff;
+    border-radius: 5px;
+  }
+
+  .cancel-button {
+    background-color: red;
+  }
+
+  .post-button {
+    background-color: #026aab;
+  }
+`;
 export default Navbar;
